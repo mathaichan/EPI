5d7e6b65d228a2e53b553de522dd1c03c6a82787
mathaiyeldo <baislmathai96@gmail.com>
2021-04-14 20:12:40 +0530

    Parity Over

--- parity.c
+++ parity.c
@@ -1,3 +1,16 @@
+#include <stdio.h>
+
 void main(){
-    in
+    int x;
+    unsigned char res=0,count=0;
+    scanf("%d",&x);
+        
+    while (x)
+    {
+        res^=1;
+        ++count;
+        x&=x-1;
+    }
+    printf("parity %d count %d",res,count);
+
 }
